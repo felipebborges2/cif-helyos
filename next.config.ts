7aa44328ddfb96f25ec90849d358d8b4c3162e6b
Felipe Borges <fisfelipeborges@gmail.com>
2026-06-10 00:43:27 -0300

    Disable router cache for dynamic pages to reflect team changes instantly

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  experimental: {
+    staleTimes: {
+      dynamic: 0,
+    },
+  },
 };
 
 export default nextConfig;
